75a05ca437379fa40821f9fcc779a1bace841c5d
Bekki Young <moonbladez@hotmail.com>
2019-08-06 13:39:48 +0200

    first page

---
+++ src/constants/social-icons.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitterSquare, FaSquarespace } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook></FaFacebook>,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaTwitterSquare></FaTwitterSquare>,
+    url: "https://twitter.com",
+  },
+  {
+    icon: <FaSquarespace></FaSquarespace>,
+    url: "https://twitter.com",
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="hunt by meowing loudly at 5am next to human slave"
+      >
+        <Link to="/tours" className="btn-white">
+          Explore Tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    <About></About>
+    <Services></Services>
+  </Layout>
+)
