80c7a197f3fe8e29aab99f0c33d97ab5b08d1819
Bekki Young <moonbladez@hotmail.com>
2019-08-07 20:18:34 +0200

    tour page added

---
+++ src/components/Home/FeaturedTours.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import Tour from "../Tours/Tour"
+import { useStaticQuery, graphql } from "gatsby"
+import Title from "../Title"
+import styles from "../../css/items.module.css"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+const getFeaturedTours = graphql`
+  {
+    featuredTours: allContentfulTour(filter: { featured: { eq: true } }) {
+      edges {
+        node {
+          name
+          price
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const FeaturedTours = () => {
+  const response = useStaticQuery(getFeaturedTours)
+  const tours = response.featuredTours.edges
+
+  return (
+    <section className={styles.tours}>
+      <Title title="featured" subtitle="tours"></Title>
+      <div className={styles.center}>
+        {tours.map(({ node }) => {
+          return <Tour key={node.contentful_id} tour={node}></Tour>
+        })}
+      </div>
+      <AniLink fade to="/tours" className={"btn-primary"}>
+        all tours
+      </AniLink>
+    </section>
+  )
+}
+
+export default FeaturedTours
---
+++ src/components/Tours/Tours.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import TourList from "./TourList"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getTours = graphql`
+  {
+    tours: allContentfulTour {
+      edges {
+        node {
+          name
+          price
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Tours = () => {
+  const { tours } = useStaticQuery(getTours)
+  return <TourList tours={tours}></TourList>
+}
+
+export default Tours
