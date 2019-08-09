a8f0e013f7cf0e04dcffee0ff412d5e81ccdeee5
Bekki Young <moonbladez@hotmail.com>
2019-08-09 16:35:10 +0200

    ended twitter card

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     description: "Explore awesome tours",
     author: "@Buludesigns",
     twitterUsername: "@jegheterbekki",
-    image: "defaultBcg.jpeg",
+    image: "/puppy.jpeg",
     url: "https://backroads-explorer.netlify.com"
   },
   plugins: [
